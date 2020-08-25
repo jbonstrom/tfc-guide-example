08047a0069dc194dc0a85691e6d665840f979438
jbonstrom <jamesbonstrom@gmail.com>
2020-08-25 17:55:21 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,3 +19,8 @@ resource "google_compute_instance" "vm_instance" {
     }
   }
 }
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
