b8e04ce16933d2c0f7f37caf37cfabeb8500ebf6
jbonstrom <jamesbonstrom@gmail.com>
2020-08-25 17:51:20 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 provider "google" {
-  credentials = var.credsvar
   project = "web-project-dev"
   region = "us-west1"
 }
 
 resource "google_compute_instance" "vm_instance" {
   name = "testinstance"
-  machine_type = "n1-standard-1"
+  machine_type = "n1-standard"
   
   boot_disk {
 	initialze_params {
 		image = "centos/centos-7"
+		}
 	}
 }
